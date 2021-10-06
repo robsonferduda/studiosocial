3174f80422d57dd094c193df5a14b2ff54faf3fe
rafael01costa <rafael01costa@gmail.com>
2021-10-06 17:20:29 -0300

    teste

--- app/Classes/HashTag.php
+++ app/Classes/HashTag.php
@@ -11,7 +11,7 @@ class HashTag{
 
     public function pullMedias()
     {
-        $access_token = "EAAInyDkHeeYBAEZCgeoBYmJfW3EKudy0WT3OwsWMODZCBKdqoQZAW0euyYz9pZAaZAlSAICZB5F5oR24GWvatSUE2BDUHoDVVYTZBAYptWDxLwrHjDatfqzTwo5afdRpZC7EphTnBcZCMVw9yXg32ySuzwEqcWiiZBWpyChZBNqN8WOXwZDZD";
+        $access_token = "EAAInyDkHeeYBAL4jeYYQqzc3FS6AihHneRc2AlleFJgNPPkxljZAZCerI6Jfu5KpMHp7ZAUTb7HkFeBhRZALUlfUeME4tVJFCQGfJ2BdLRoMI6I5EpPaXowX3aXaNcfEZCvrUd1GTVReP2ZA5OrOWUQOreoQiWDnjsqEv4IQgl9OR7QmpGTRUT";
         $id_user_id = '17841437726599322';
         $after = '';
 
