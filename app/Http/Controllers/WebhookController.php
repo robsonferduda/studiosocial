ec10bca65fa2bace80227b9e490e0171d1960bc9
rafael01costa <rafael01costa@gmail.com>
2021-10-15 16:23:31 -0300

    webhook teste

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -3,13 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class WebhookController extends Controller
 {
 
     public function receive(Request $request)
     {
-        \Log::info($request);
+        Log::info($request);
 
         return $request->hub_challenge;
 
