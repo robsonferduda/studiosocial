ae27014c88fc1634ff0d5078907b5fb1df078668
Rafael <rafael@coperve.ufsc.br>
2021-10-19 15:49:33 -0300

    Mudança de nomenclatura

--- app/Http/Controllers/IGWebhookController.php
+++ app/Http/Controllers/IGWebhookController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 
-class WebhookController extends Controller
+class IGWebhookController extends Controller
 {
 
     public function receive(Request $request)
--- routes/web.php
+++ routes/web.php
@@ -27,7 +27,7 @@ Route::get('login/facebook', 'FacebookController@redirectToProvider');
 Route::get('login/facebook/callback', 'FacebookController@handleProviderCallback');
 
 Route::get('check/token/{token}', 'TokenController@checkFacebookToken');
-Route::post('webhook', 'WebhookController@receive');
+Route::post('ig-webhook', 'IGWebhookController@receive');
 
 Route::get('/test-api', 'TestApiController@test');
 
