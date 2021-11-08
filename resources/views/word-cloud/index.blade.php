10242844d0dd6726ef8770cafa6f81a80c61c0db
Rafael <rafael@coperve.ufsc.br>
2021-11-08 14:17:05 -0300

    WordCloud

--- resources/views/word-cloud/index.blade.php
+++ resources/views/word-cloud/index.blade.php
@@ -14,7 +14,7 @@
         $('body').loader('show');
 
 
-        fetch('/nuvem-palavras/words', {
+        fetch('/public/nuvem-palavras/words', {
             method: 'GET', 
             headers: {
                 'Content-Type': 'application/json',
@@ -65,22 +65,6 @@
                 }
             });            
         });
-
-        // var words = [
-        //     {text: "Lorem", weight: 13},
-        //     {text: "Ipsum", weight: 10.5},
-        //     {text: "Dolor", weight: 9.4},
-        //     {text: "Sit", weight: 8},
-        //     {text: "Amet", weight: 6.2},
-        //     {text: "Consectetur", weight: 5},
-        //     {text: "Adipiscing", weight: 5},
-        // ];
-        
-        // let cloud = $('#cloud').jQCloud(words, {
-            
-        // });
-
-        //console.log(cloud);
     });
 </script>
 @endsection
