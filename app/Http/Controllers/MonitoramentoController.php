23bb7417272516f0bde56303bba5fd0e5b788b55
Robson Fernando Duda <robsonferduda@gmail.com>
2021-11-10 18:17:44 -0300

    Monitoramento - Home

--- app/Http/Controllers/MonitoramentoController.php
+++ app/Http/Controllers/MonitoramentoController.php
@@ -18,10 +18,14 @@ class MonitoramentoController extends Controller
 
     public function index()
     {
+        $totais = array();
         $client_id = Session::get('cliente')['id'];
-        $totais = array('total_insta' => Media::where('client_id',$client_id)->count(),
-                        'total_face' => FbPost::where('client_id',$client_id)->count(),
-                        'total_twitter' => MediaTwitter::where('client_id',$client_id)->count());
+
+        if($client_id){
+            $totais = array('total_insta' => Media::where('client_id',$client_id)->count(),
+                            'total_face' => FbPost::where('client_id',$client_id)->count(),
+                            'total_twitter' => MediaTwitter::where('client_id',$client_id)->count());
+        }
 
         return view('monitoramento/index', compact('totais'));
     }
@@ -29,8 +33,58 @@ class MonitoramentoController extends Controller
     public function seleciona($rede)
     {
         $client_id = Session::get('cliente')['id'];
+        $medias = array();
 
+        switch ($rede) {
+            case 'instagram':
+                $medias_temp = Media::where('client_id', $client_id)->get();
+                foreach ($medias_temp as $key => $media) {
+                    
+                    $medias[] = array('id' => $media->media_id,
+                                      'text' => $media->caption,
+                                      'username' => '',
+                                      'created_at' => $media->timestamp,
+                                      'like_count' => $media->like_count,
+                                      'comments_count' => $media->like_count,
+                                      'social_media_id' => $media->social_media_id,
+                                      'tipo' => 'instagram');
 
-        return view('monitoramento/medias');
+                }
+                break;
+
+            case 'facebook':
+                $medias_temp = FbPost::where('client_id', $client_id)->get();
+                foreach ($medias_temp as $key => $media) {
+                    
+                    $medias[] = array('id' => $media->id,
+                                      'text' => $media->message,
+                                      'username' => '',
+                                      'created_at' => $media->tagged_time,
+                                      'like_count' => $media->like_count,
+                                      'comments_count' => $media->like_count,
+                                      'social_media_id' => $media->social_media_id,
+                                     'tipo' => 'facebook');
+
+                }
+                break;
+            
+            case 'twitter':
+                $medias_temp = MediaTwitter::where('client_id', $client_id)->get();
+                foreach ($medias_temp as $key => $media) {
+                    
+                    $medias[] = array('id' => $media->twitter_id,
+                                      'text' => $media->full_text,
+                                      'username' => $media->user_name,
+                                      'created_at' => $media->created_tweet_at,
+                                      'like_count' => $media->favorite_count,
+                                      'comments_count' => 0,
+                                      'social_media_id' => $media->social_media_id,
+                                      'tipo' => 'twitter');
+
+                }
+            break;
+        }
+
+        return view('monitoramento/medias', compact('medias'));
     }
 }
--- resources/views/monitoramento/medias.blade.php
+++ resources/views/monitoramento/medias.blade.php
@@ -5,15 +5,57 @@
         <div class="card-header">
             <div class="row">
                 <div class="col-md-6">
-                    <h4 class="card-title ml-2"><i class="nc-icon nc-sound-wave"></i> Monitoramento</h4>
+                    <h4 class="card-title ml-2"><i class="fa fa-hashtag"></i> Monitoramento </h4>
                 </div>
                 <div class="col-md-6">
-                    
+                   
                 </div>
             </div>
         </div>
         <div class="card-body">
-            
+            <div class="col-md-12">
+                @include('layouts.mensagens')
+            </div>
+            <h6 class="ml-3">Total de mídias coletadas: {{ count($medias) }}</h6>
+            @foreach($medias as $key => $media)
+                <div class="card">
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-md-2 text-center">
+                                <img src="{{ url('img/user.png') }}" alt="Imagem de Perfil" class="rounded-pill">
+                                <p>{{ $media['username'] }}</p>                                
+                                @switch($media['tipo'])
+                                    @case('instagram')
+                                        <h3><i class="fa fa-instagram text-pink"></i></h3>
+                                        @break
+                                    @case('twitter')
+                                        <h1 class="text-center"><i class="fa fa-twitter text-info"></i></h1>
+                                        @break
+                                    @default                                        
+                                @endswitch
+                            </div>
+                            <div class="col-md-10">
+                                <div class="mb-2">
+                                    <span class="badge badge-pill badge-primary">
+                                        <i class="fa fa-thumbs-up"></i> {{ $media['like_count'] }}
+                                    </span>
+                                    <span class="badge badge-pill badge-danger">
+                                        <i class="fa fa-comments"></i> {{ $media['comments_count'] }}
+                                    </span>   
+                                    <span class="badge badge-pill badge-success">
+                                        <i class="fa fa-share"></i> {{ $media['comments_count'] }}
+                                    </span> 
+                                    <span class="badge badge-pill badge-info">
+                                        <i class="fa fa-users"></i> {{ $media['comments_count'] }}
+                                    </span> 
+                                    <span class="float-right">{{ Carbon\Carbon::parse($media['created_at'])->format('d/m/Y H:i') }}</span>
+                                </div>
+                                <p>{{ $media['text'] }}</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
         </div>
     </div>
 </div> 
