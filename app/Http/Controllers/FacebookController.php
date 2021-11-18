8c71d1c11e3452e515f7c6ea2da37007a34fa78b
rafael01costa <rafael01costa@gmail.com>
2021-11-18 07:57:37 -0300

    Teste

--- app/Http/Controllers/FacebookController.php
+++ app/Http/Controllers/FacebookController.php
@@ -64,9 +64,7 @@ class FacebookController extends Controller
                     ]
                 );
 
-                $teste = $this->subscribeApps($fbPage['id'], $fbPage['access_token']);
-
-                dd($teste->json());
+                $this->subscribeApps($fbPage['id'], $fbPage['access_token']);
 
                 $ig_business_account = $this->getIGBusinessAccount($fb_page->page_id, $fb_account->token);
 
