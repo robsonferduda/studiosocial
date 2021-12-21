fdc5281125423c6a2dda76e154119cb9c6e5c8fd
Rafael <rafael@coperve.ufsc.br>
2021-12-21 08:35:45 -0300

    teste wordcloud

--- app/Http/Controllers/WordCloudController.php
+++ app/Http/Controllers/WordCloudController.php
@@ -2,10 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Rule;
 use App\WordsExecption;
 use Illuminate\Support\Facades\Session;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Storage;
 use Symfony\Component\Process\Process;
 
 class WordCloudController extends Controller
@@ -40,30 +40,47 @@ class WordCloudController extends Controller
     }
 
     public function getWords() 
-    {
-
+    {   
+        ini_set('memory_limit', '2048M');
+        
         if(isset($this->cliente['id'])) {
 
-            $file = Storage::disk('wordcloud')->get("cliente-{$this->cliente['id']}-wordclould.json");
+            $rules = Rule::where('client_id', $this->cliente['id'])->get();
+          
+            $words = [];
 
-            $words = json_decode($file);
-            
-            $words = (Array) $words;
-            
-            arsort($words);
-    
-            $words = array_slice($words, 0, 200);
-    
-            $word_cloud = [];
-            foreach($words as $word => $qtd_times) {
-                $word_cloud[$word] = $qtd_times;  
+            foreach($rules as $rule) {
+                foreach($rule->igPosts as $post){
+                    $words_post = json_decode($post->wordcloud_expression, true);
+
+                    if(!empty($words_post)) {
+                        foreach($words_post as $key => $value) {
+                            $words[] = $key;
+                        }
+                    }                   
+                }
+
+                foreach($rule->twPosts as $post){
+                    $words_post = json_decode($post->wordcloud_expression, true);
+
+                    if(!empty($words_post)) {
+                        foreach($words_post as $key => $value) {
+                            $words[] = $key;
+                        }
+                    }                   
+                }
             }
 
+            $lista_frequencia = array_count_values($words);
+            arsort($lista_frequencia);
+    
+            $lista_frequencia = array_slice($lista_frequencia, 0, 200);
+    
         } else {
-            $word_cloud = ['Cliente' => 3, 'Não' => 2, 'Selecionado' => 2];
+            $lista_frequencia = ['Cliente' => 3, 'Não' => 2, 'Selecionado' => 2];
         }
         
-        echo json_encode($word_cloud);
+        echo json_encode($lista_frequencia);
 
     }
 }
