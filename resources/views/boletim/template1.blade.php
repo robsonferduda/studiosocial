03d8b60f0277efc3599f34f501ad5be7dffc159c
Robson Fernando Duda <robsonferduda@gmail.com>
2021-12-23 23:51:27 -0300

    Layout Email

--- resources/views/boletim/template1.blade.php
+++ resources/views/boletim/template1.blade.php
@@ -1,4 +1,7 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"><html xmlns="http://www.w3.org/1999/xhtml" xmlns:o="urn:schemas-microsoft-com:office:office" style="width:100%;font-family:lato, 'helvetica neue', helvetica, arial, sans-serif;-webkit-text-size-adjust:100%;-ms-text-size-adjust:100%;padding:0;Margin:0"><head><meta http-equiv="Content-Security-Policy" content="script-src 'none'; connect-src 'none'; object-src 'none'; form-action 'none';"> 
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xmlns:o="urn:schemas-microsoft-com:office:office" style="width:100%;font-family:lato, 'helvetica neue', helvetica, arial, sans-serif;-webkit-text-size-adjust:100%;-ms-text-size-adjust:100%;padding:0;Margin:0">
+<head>
+    <meta http-equiv="Content-Security-Policy" content="script-src 'none'; connect-src 'none'; object-src 'none'; form-action 'none';"> 
     <meta charset="UTF-8"> 
     <meta content="width=device-width, initial-scale=1" name="viewport"> 
     <meta name="x-apple-disable-message-reformatting"> 
@@ -23,48 +26,56 @@
   </xml>
   <![endif]--> 
     <link rel="shortcut icon" type="image/png" href="https://stripo.email/assets/img/favicon.png"> 
-    <style type="text/css">
-  #outlook a {
-      padding:0;
-  }
-  .ExternalClass {
-      width:100%;
-  }
-  .ExternalClass,
-  .ExternalClass p,
-  .ExternalClass span,
-  .ExternalClass font,
-  .ExternalClass td,
-  .ExternalClass div {
-      line-height:100%;
-  }
-  .es-button {
-      mso-style-priority:100!important;
-      text-decoration:none!important;
-  }
-  a[x-apple-data-detectors] {
-      color:inherit!important;
-      text-decoration:none!important;
-      font-size:inherit!important;
-      font-family:inherit!important;
-      font-weight:inherit!important;
-      line-height:inherit!important;
-  }
-  .es-desk-hidden {
-      display:none;
-      float:left;
-      overflow:hidden;
-      width:0;
-      max-height:0;
-      line-height:0;
-      mso-hide:all;
-  }
-  .CToWUd{
-    width: 20px;
-    margin-bottom: -3px;
-  }
-  @media only screen and (max-width:800px) {p, ul li, ol li, a { font-size:16px!important; line-height:150%!important } h1 { font-size:30px!important; text-align:center; line-height:120%!important } h2 { font-size:26px!important; text-align:center; line-height:120%!important } h3 { font-size:20px!important; text-align:center; line-height:120%!important } h1 a { font-size:30px!important } h2 a { font-size:26px!important } h3 a { font-size:20px!important } .es-menu td a { font-size:16px!important } .es-header-body p, .es-header-body ul li, .es-header-body ol li, .es-header-body a { font-size:16px!important } .es-footer-body p, .es-footer-body ul li, .es-footer-body ol li, .es-footer-body a { font-size:16px!important } .es-infoblock p, .es-infoblock ul li, .es-infoblock ol li, .es-infoblock a { font-size:12px!important } *[class="gmail-fix"] { display:none!important } .es-m-txt-c, .es-m-txt-c h1, .es-m-txt-c h2, .es-m-txt-c h3 { text-align:center!important } .es-m-txt-r, .es-m-txt-r h1, .es-m-txt-r h2, .es-m-txt-r h3 { text-align:right!important } .es-m-txt-l, .es-m-txt-l h1, .es-m-txt-l h2, .es-m-txt-l h3 { text-align:left!important } .es-m-txt-r img, .es-m-txt-c img, .es-m-txt-l img { display:inline!important } .es-button-border { display:block!important } a.es-button { font-size:20px!important; display:block!important; border-width:15px 25px 15px 25px!important } .es-btn-fw { border-width:10px 0px!important; text-align:center!important } .es-adaptive table, .es-btn-fw, .es-btn-fw-brdr, .es-left, .es-right { width:100%!important } .es-content table, .es-header table, .es-footer table, .es-content, .es-footer, .es-header { width:100%!important; max-width:800px!important } .es-adapt-td { display:block!important; width:100%!important } .adapt-img { width:100%!important; height:auto!important } .es-m-p0 { padding:0px!important } .es-m-p0r { padding-right:0px!important } .es-m-p0l { padding-left:0px!important } .es-m-p0t { padding-top:0px!important } .es-m-p0b { padding-bottom:0!important } .es-m-p20b { padding-bottom:20px!important } .es-mobile-hidden, .es-hidden { display:none!important } tr.es-desk-hidden, td.es-desk-hidden, table.es-desk-hidden { width:auto!important; overflow:visible!important; float:none!important; max-height:inherit!important; line-height:inherit!important } tr.es-desk-hidden { display:table-row!important } table.es-desk-hidden { display:table!important } td.es-desk-menu-hidden { display:table-cell!important } .es-menu td { width:1%!important } table.es-table-not-adapt, .esd-block-html table { width:auto!important } table.es-social { display:inline-block!important } table.es-social td { display:inline-block!important } }
-  </style> 
+    <style>
+        #outlook a {
+            padding:0;
+        }
+        .ExternalClass {
+            width:100%;
+        }
+        .ExternalClass,
+        .ExternalClass p,
+        .ExternalClass span,
+        .ExternalClass font,
+        .ExternalClass td,
+        .ExternalClass div {
+            line-height:100%;
+        }
+        .es-button {
+            mso-style-priority:100!important;
+            text-decoration:none!important;
+        }
+        a[x-apple-data-detectors] {
+            color:inherit!important;
+            text-decoration:none!important;
+            font-size:inherit!important;
+            font-family:inherit!important;
+            font-weight:inherit!important;
+            line-height:inherit!important;
+        }
+        .es-desk-hidden {
+            display:none;
+            float:left;
+            overflow:hidden;
+            width:0;
+            max-height:0;
+            line-height:0;
+            mso-hide:all;
+        }
+        p > img{
+          width: 19px;
+          position: relative;
+          left: 1px;
+          top: 2px;
+        }
+        img.CToWUd {
+          width: 20px;
+          position: relative;
+          left: 1px;
+          top: 2px;
+        }
+        @media only screen and (max-width:800px) {p, ul li, ol li, a { font-size:16px!important; line-height:150%!important } h1 { font-size:30px!important; text-align:center; line-height:120%!important } h2 { font-size:26px!important; text-align:center; line-height:120%!important } h3 { font-size:20px!important; text-align:center; line-height:120%!important } h1 a { font-size:30px!important } h2 a { font-size:26px!important } h3 a { font-size:20px!important } .es-menu td a { font-size:16px!important } .es-header-body p, .es-header-body ul li, .es-header-body ol li, .es-header-body a { font-size:16px!important } .es-footer-body p, .es-footer-body ul li, .es-footer-body ol li, .es-footer-body a { font-size:16px!important } .es-infoblock p, .es-infoblock ul li, .es-infoblock ol li, .es-infoblock a { font-size:12px!important } *[class="gmail-fix"] { display:none!important } .es-m-txt-c, .es-m-txt-c h1, .es-m-txt-c h2, .es-m-txt-c h3 { text-align:center!important } .es-m-txt-r, .es-m-txt-r h1, .es-m-txt-r h2, .es-m-txt-r h3 { text-align:right!important } .es-m-txt-l, .es-m-txt-l h1, .es-m-txt-l h2, .es-m-txt-l h3 { text-align:left!important } .es-m-txt-r img, .es-m-txt-c img, .es-m-txt-l img { display:inline!important } .es-button-border { display:block!important } a.es-button { font-size:20px!important; display:block!important; border-width:15px 25px 15px 25px!important } .es-btn-fw { border-width:10px 0px!important; text-align:center!important } .es-adaptive table, .es-btn-fw, .es-btn-fw-brdr, .es-left, .es-right { width:100%!important } .es-content table, .es-header table, .es-footer table, .es-content, .es-footer, .es-header { width:100%!important; max-width:800px!important } .es-adapt-td { display:block!important; width:100%!important } .adapt-img { width:100%!important; height:auto!important } .es-m-p0 { padding:0px!important } .es-m-p0r { padding-right:0px!important } .es-m-p0l { padding-left:0px!important } .es-m-p0t { padding-top:0px!important } .es-m-p0b { padding-bottom:0!important } .es-m-p20b { padding-bottom:20px!important } .es-mobile-hidden, .es-hidden { display:none!important } tr.es-desk-hidden, td.es-desk-hidden, table.es-desk-hidden { width:auto!important; overflow:visible!important; float:none!important; max-height:inherit!important; line-height:inherit!important } tr.es-desk-hidden { display:table-row!important } table.es-desk-hidden { display:table!important } td.es-desk-menu-hidden { display:table-cell!important } .es-menu td { width:1%!important } table.es-table-not-adapt, .esd-block-html table { width:auto!important } table.es-social { display:inline-block!important } table.es-social td { display:inline-block!important } }
+      </style> 
    </head> 
    <body style="width:100%;font-family:lato, 'helvetica neue', helvetica, arial, sans-serif;-webkit-text-size-adjust:100%;-ms-text-size-adjust:100%;padding:0;Margin:0"> 
     <div class="es-wrapper-color" style="background-color:#F4F4F4"> 
@@ -175,21 +186,25 @@
                                             @case('web')
                                                 @php
                                                     $tipo_formatado = 'Web';
+                                                    $icone = 'web';
                                                 @endphp
                                             @break
                                             @case('tv')
                                                 @php
                                                     $tipo_formatado = 'TV';
+                                                    $icone = 'tv';
                                                 @endphp
                                             @break
                                             @case('radio')
                                                 @php
                                                     $tipo_formatado = 'Rádio';
+                                                    $icone = 'radio';
                                                 @endphp
                                             @break
                                             @case('jornal')
                                                 @php
                                                     $tipo_formatado = 'Jornal';
+                                                    $icone = 'jornal';
                                                 @endphp
                                             @break
                                             @default
@@ -198,10 +213,14 @@
                                                 @endphp
                                             @break                                    
                                         @endswitch
-                                        <p style="text-transform: uppercase; font-weight: 600;">
-                                          <img class="icone" src="https://studiosocial.ga/img/icone_tv.png">
-                                          {!! $tipo_formatado !!}
-                                        </p>
+                                        <div style="text-transform: uppercase; font-weight: 600;">
+                                          <table>
+                                            <tr>
+                                              <td><img class="icone" src="https://studiosocial.ga/img/icone_{{ $icone }}.png"></td>
+                                              <td><p>{!! $tipo_formatado !!}</p></td>
+                                            </tr>
+                                          </table> 
+                                        </div>
                                     @endif
     
                                     @if($noticia->clipagem == 'tv')
