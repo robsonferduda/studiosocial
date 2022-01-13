76b988016f02b577a8e159dc2ce2b7b915bd29dd
Robson Fernando Duda <robsonferduda@gmail.com>
2022-01-13 09:56:37 -0300

    Auditoria - Listagem

--- resources/views/auditoria/index.blade.php
+++ resources/views/auditoria/index.blade.php
@@ -32,7 +32,7 @@
                             <tr>
                                 <td>{{ date('d/m/Y H:i:s', strtotime($audit->created_at)) }}</td>
                                 <td>
-                                    @if(isset($audit->user->roles()))
+                                    @if(!empty($audit->user->roles()))
                                         @forelse($audit->user->roles()->get() as $role)
                                             <span class="badge badge-{{ $role->display_color }}">{{ $role->display_name }}</span>
                                         @empty
