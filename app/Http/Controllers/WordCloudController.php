30b007337280a0d5d06e2610f389d8a81a37db81
rafael01costa <rafael01costa@gmail.com>
2022-01-16 18:24:34 -0300

    Wordcloud tentativa 10000000000

--- app/Http/Controllers/WordCloudController.php
+++ app/Http/Controllers/WordCloudController.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers;
 
 use App\Rule;
+use App\WordCloudText;
 use App\WordsExecption;
 use Illuminate\Support\Facades\Session;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 use Laracasts\Flash\Flash;
+use Symfony\Component\Process\Exception\ProcessFailedException;
 use Symfony\Component\Process\Process;
 
 class WordCloudController extends Controller
@@ -73,25 +75,71 @@ class WordCloudController extends Controller
     {   
         if(isset($this->cliente['id'])) {
 
-            $words_execption = WordsExecption::where('client_id', $this->cliente['id'])->pluck('word')->toArray();
+            $rule = Rule::where('id', $rule)->first();
 
-            $file = Storage::disk('wordcloud')->get("cliente-{$this->cliente['id']}-rule-{$rule}-wordclould.json");
-            //dd($words_execption);
+            $igPosts = $rule->igPosts()->pluck('caption')->toArray();
+            $igComments = $rule->igComments()->pluck('text')->toArray();
+            $fbPosts = $rule->fbPosts()->pluck('message')->toArray();
+            $fbComments = $rule->fbComments()->pluck('text')->toArray();
+            $twPosts = $rule->twPosts()->pluck('full_text')->toArray();
 
-            $words = json_decode($file);
-            $words = (Array) $words;
-            arsort($words);
+            $textig = $this->concatenateSanitizeText($igPosts);
+            $textigc = $this->concatenateSanitizeText($igComments);
+            $textfb = $this->concatenateSanitizeText($fbPosts);
+            $textfbc = $this->concatenateSanitizeText($fbComments);
+            $texttw = $this->concatenateSanitizeText($twPosts);
 
-            $words = array_slice($words, 0, 200);
+            $text = $textig.' '.$textigc.' '.$textfb.' '.$textfbc.' '.$texttw;
 
-            $word_cloud = [];
-            foreach($words as $word => $qtd_times) {
-                if(in_array($word, $words_execption)){
-                    continue;
-                }
+            $wordcloud_text = WordCloudText::create([
+                'text' => $text
+            ]);
 
-                $word_cloud[$word] = $qtd_times;  
+
+            $file_name = 'wordcloud-'.strtotime(now()).'.json';
+
+            if(isset($wordcloud_text->id)) {
+                
+                $word_cloud = [];
+
+                $process = new Process(['python3', base_path().'/studio-social-wordcloud-rules.py', $wordcloud_text->id, $file_name]);
+                $process->run(function ($type, $buffer) use ($file_name, &$word_cloud){
+                    if (Process::ERR === $type) {
+                        //echo 'ERR > '.$buffer.'<br />';
+                    } else {
+                        
+                        if(trim($buffer) == 'END') {
+                            //echo 'OUT > '.$buffer.'<br />';
+
+                            $file = Storage::disk('wordcloud')->get($file_name);
+                            //dd($words_execption);
+                
+                            $words = json_decode($file);
+                           
+                            $words = (Array) $words;
+                            arsort($words);
+                
+                            $words = array_slice($words, 0, 200);
+
+                            $words_execption = WordsExecption::where('client_id', $this->cliente['id'])->pluck('word')->toArray();
+                                            
+                            foreach($words as $word => $qtd_times) {
+
+                                if(in_array($word, $words_execption))
+                                    continue;
+
+                                $word_cloud[$word] = $qtd_times;  
+                            }
+
+                            Storage::disk('wordcloud')->delete($file_name);
+                        }
+    
+                    }
+                });
+            
             }
+
+            
 
         } else {
                 $word_cloud = ['Cliente' => 3, 'Não' => 2, 'Selecionado' => 2];               
@@ -100,6 +148,20 @@ class WordCloudController extends Controller
         echo json_encode($word_cloud);
         
     }
+
+    public function concatenateSanitizeText(Array $textArray)
+    {
+        $concatenateText = '';
+
+        foreach($textArray as $text) {
+                        
+            $concatenateText .= ' '.$text;
+        }
+
+        return $concatenateText;
+
+    }
+
 
     public function excecoes()
     {
