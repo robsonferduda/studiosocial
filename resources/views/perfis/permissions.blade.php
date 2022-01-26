e6c96d5ba998e62101a7986863f071e14761bf3d
Robson Fernando Duda <robsonferduda@gmail.com>
2022-01-26 15:50:02 -0300

    Permissions

--- resources/views/perfis/permissions.blade.php
+++ resources/views/perfis/permissions.blade.php
@@ -5,7 +5,10 @@
         <div class="card-header">
             <div class="row">
                 <div class="col-md-6">
-                    <h4 class="card-title"><i class="fa fa-group"></i> Perfis > Permissões</h4>
+                    <h4 class="card-title">
+                        <i class="fa fa-group"></i> Perfis
+                        <i class="fa fa-angle-double-right" aria-hidden="true"></i> Permissões 
+                    </h4>
                 </div>
                 <div class="col-md-6">
                     <a href="{{ url('perfis') }}" class="btn btn-primary pull-right" style="margin-right: 12px;"><i class="fa fa-group"></i> Perfis</a>
@@ -13,15 +16,25 @@
             </div>
         </div>
         <div class="card-body">
-            <div class="row">
-                <div class="col-md-12">
-                    @include('layouts.mensagens')
-                </div>
+            <div class="col-md-12">
+                @include('layouts.mensagens')
             </div>
-            @foreach($role->permissions as $key => $permission)
-                <p>{{ $permission->display_name }}</p>
-            @endforeach
-            
+            <div class="col-md-12">
+                <table class="table">
+                    <thead>
+                    <tr>
+                        <th>Permissão</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                        @foreach($role->permissions as $key => $permission)
+                            <tr>
+                                <td>{{ $permission->display_name }}</td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>  
+            </div>                     
         </div>
     </div>
 </div> 
