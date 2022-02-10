b63c7e031ba7f07a1ff2c766c86f118df1dc0047
Robson Fernando Duda <robsonferduda@gmail.com>
2022-02-10 20:30:15 -0300

    Boletim

--- app/Http/Controllers/BoletimController.php
+++ app/Http/Controllers/BoletimController.php
@@ -50,40 +50,60 @@ class BoletimController extends Controller
     public function enviar($id)
     {
         $boletim = Boletim::where('id', $id)->first();
-        $lista_email = array(
-                        array('nome' => 'Álvaro Lista', 'email' => 'alvaro@studioclipagem.com.br'),
-                        array('nome' => 'Rafael de Moraes Costa', 'email' => 'rafael01costa@gmail.com'),
-                        array('nome' => 'Robson Fernando Duda', 'email' => 'robsonferduda@gmail.com'),
-                        array('nome' => 'Adrian Elkuch', 'email' => 'adrian.elkuch@zurich-airport.lat'),
-                        array('nome' => 'Anderson Pinheiro', 'email' => 'anderson.pinheiro@zurichairportbrasil.com'),
-                        array('nome' => 'Andrea Lima', 'email' => 'andrea.lima@aseb-airport.com'),
-                        array('nome' => 'Anderson Ribeiro', 'email' => 'anderson.ribeiro@zurichairportbrasil.com'),
-                        array('nome' => 'Andre Couto', 'email' => 'andre.couto@zurichaiportbrasil.com'),
-                        array('nome' => 'Bruna Fischer', 'email' => 'bruna.fischer@zurichairportbrasil.com'),
-                        array('nome' => 'Cris Vieira', 'email' => 'cris.vieira@zurichairportbrasil.com'),
-                        array('nome' => 'Davi Piza', 'email' => 'davi.piza@zurichairportbrasil.com'),
-                        array('nome' => 'Fabio Marques', 'email' => 'fabio.marques@zurichairportbrasil.com'),
-                        array('nome' => 'Fernando Castro', 'email' => 'fernando.castro@zurichairportbrasil.com'),
-                        array('nome' => 'Giovani Montibeller ', 'email' => 'giovani.montibeller@zurichairportbrasil.com'),
-                        array('nome' => 'Gustavo Brighenti', 'email' => 'gustavo.brighenti@zurichairportbrasil.com'),
-                        array('nome' => 'Jasmine Reis', 'email' => 'jasmine.reis@zurichairportbrasil.com'),
-                        array('nome' => 'Johann Gigl', 'email' => 'johann.gigl@zurich-airport.lat'),
-                        array('nome' => 'Jerco', 'email' => 'jerco.bacic@zurichairportbrasil.com'),
-                        array('nome' => 'Karen Bonfim', 'email' => 'karen.bonfim1@gmail.com'),
-                        array('nome' => 'Kleyton Mendes', 'email' => 'kleyton.mendes@zurichairportbrasil.com'),
-                        array('nome' => 'Lisiane Karan', 'email' => 'lisiane.karan@zurichairportbrasil.com'),
-                        array('nome' => 'Neuza Wagner', 'email' => 'neuza.wagner@zurichairportbrasil.com'),
-                        array('nome' => 'Michel Jung', 'email' => 'michel.jung@zurichairportbrasil.com'),
-                        array('nome' => 'Renan Barcelos', 'email' => 'renan.barcelos@zurichairportbrasil.com'),
-                        array('nome' => 'Ricardo Bresolin', 'email' => 'ricardo.bresolin@zurichairportbrasil.com'),
-                        array('nome' => 'Ricardo Gesse', 'email' => 'ricardo.gesse@zurichairportbrasil.com'),
-                        array('nome' => 'Simon Locher', 'email' => 'simon.locher@zurichairportbrasil.com'),
-                        array('nome' => 'Tamara Oliveira', 'email' => 'tamara.oliveira@zurichairportbrasil.com'),
-                        array('nome' => 'Tobias Markert', 'email' => 'tobias.markert@zurich-airport.lat'),
-                        array('nome' => 'Tobias Markert', 'email' => 'tobias.markert@zurichairportbrasil.com'),
-                        array('nome' => 'Vanessa Bezerra', 'email' => 'vanessa.bezerra@zurichairportbrasil.com'),
-                        array('nome' => 'Wilson', 'email' => 'wilson.victer@zurichairportbrasil.com')
-                    );
+
+        if($boletim->id_cliente = 452){
+
+            $lista_email = array(
+                array('nome' => 'Álvaro Lista', 'email' => 'alvaro@studioclipagem.com.br'),
+                array('nome' => 'Rafael de Moraes Costa', 'email' => 'rafael01costa@gmail.com'),
+                array('nome' => 'Robson Fernando Duda', 'email' => 'robsonferduda@gmail.com'),
+                array('nome' => 'Andre Couto', 'email' => 'andre.couto@zurichairportbrasil.com'),
+                array('nome' => 'Armstron', 'email' => 'armstron.carvalho@zurichairportbrasil.com'),
+                array('nome' => 'Cris Vieira', 'email' => 'cris.vieira@zurichairportbrasil.com'),
+                array('nome' => 'Fabio Marques', 'email' => 'fabio.marques@zurichairportbrasil.com'),
+                array('nome' => 'Kleyton Mendes', 'email' => 'kleyton.mendes@zurichairportbrasil.com'),
+                array('nome' => 'Natalia Santos Pereira', 'email' => 'natalia.pereira@zurichairportbrasil.com'),
+                array('nome' => 'Ricardo Gesse', 'email' => 'ricardo.gesse@zurichairportbrasil.com '),
+                array('nome' => 'Vanessa Bezerra', 'email' => 'vanessa.bezerra@zurichairportbrasil.com')
+            );
+
+        }else{
+
+            $lista_email = array(
+                            array('nome' => 'Álvaro Lista', 'email' => 'alvaro@studioclipagem.com.br'),
+                            array('nome' => 'Rafael de Moraes Costa', 'email' => 'rafael01costa@gmail.com'),
+                            array('nome' => 'Robson Fernando Duda', 'email' => 'robsonferduda@gmail.com'),
+                            array('nome' => 'Adrian Elkuch', 'email' => 'adrian.elkuch@zurich-airport.lat'),
+                            array('nome' => 'Anderson Pinheiro', 'email' => 'anderson.pinheiro@zurichairportbrasil.com'),
+                            array('nome' => 'Andrea Lima', 'email' => 'andrea.lima@aseb-airport.com'),
+                            array('nome' => 'Anderson Ribeiro', 'email' => 'anderson.ribeiro@zurichairportbrasil.com'),
+                            array('nome' => 'Andre Couto', 'email' => 'andre.couto@zurichaiportbrasil.com'),
+                            array('nome' => 'Bruna Fischer', 'email' => 'bruna.fischer@zurichairportbrasil.com'),
+                            array('nome' => 'Cris Vieira', 'email' => 'cris.vieira@zurichairportbrasil.com'),
+                            array('nome' => 'Davi Piza', 'email' => 'davi.piza@zurichairportbrasil.com'),
+                            array('nome' => 'Fabio Marques', 'email' => 'fabio.marques@zurichairportbrasil.com'),
+                            array('nome' => 'Fernando Castro', 'email' => 'fernando.castro@zurichairportbrasil.com'),
+                            array('nome' => 'Giovani Montibeller ', 'email' => 'giovani.montibeller@zurichairportbrasil.com'),
+                            array('nome' => 'Gustavo Brighenti', 'email' => 'gustavo.brighenti@zurichairportbrasil.com'),
+                            array('nome' => 'Jasmine Reis', 'email' => 'jasmine.reis@zurichairportbrasil.com'),
+                            array('nome' => 'Johann Gigl', 'email' => 'johann.gigl@zurich-airport.lat'),
+                            array('nome' => 'Jerco', 'email' => 'jerco.bacic@zurichairportbrasil.com'),
+                            array('nome' => 'Karen Bonfim', 'email' => 'karen.bonfim1@gmail.com'),
+                            array('nome' => 'Kleyton Mendes', 'email' => 'kleyton.mendes@zurichairportbrasil.com'),
+                            array('nome' => 'Lisiane Karan', 'email' => 'lisiane.karan@zurichairportbrasil.com'),
+                            array('nome' => 'Neuza Wagner', 'email' => 'neuza.wagner@zurichairportbrasil.com'),
+                            array('nome' => 'Michel Jung', 'email' => 'michel.jung@zurichairportbrasil.com'),
+                            array('nome' => 'Renan Barcelos', 'email' => 'renan.barcelos@zurichairportbrasil.com'),
+                            array('nome' => 'Ricardo Bresolin', 'email' => 'ricardo.bresolin@zurichairportbrasil.com'),
+                            array('nome' => 'Ricardo Gesse', 'email' => 'ricardo.gesse@zurichairportbrasil.com'),
+                            array('nome' => 'Simon Locher', 'email' => 'simon.locher@zurichairportbrasil.com'),
+                            array('nome' => 'Tamara Oliveira', 'email' => 'tamara.oliveira@zurichairportbrasil.com'),
+                            array('nome' => 'Tobias Markert', 'email' => 'tobias.markert@zurich-airport.lat'),
+                            array('nome' => 'Tobias Markert', 'email' => 'tobias.markert@zurichairportbrasil.com'),
+                            array('nome' => 'Vanessa Bezerra', 'email' => 'vanessa.bezerra@zurichairportbrasil.com'),
+                            array('nome' => 'Wilson', 'email' => 'wilson.victer@zurichairportbrasil.com')
+                        );
+        }
 
         return view('boletim/lista-envio', compact('boletim', 'lista_email'));
         /*
--- resources/views/boletim/detalhes.blade.php
+++ resources/views/boletim/detalhes.blade.php
@@ -4,16 +4,16 @@
     <div class="card">
         <div class="card-header">
             <div class="row">
-                <div class="col-md-8">
+                <div class="col-md-7">
                     <h4 class="card-title ml-2">
                         <i class="fa fa-file-o"></i> Boletim 
                         <i class="fa fa-angle-double-right" aria-hidden="true"></i> {{ $boletim->titulo }}
                     </h4>
                 </div>
-                <div class="col-md-4">
+                <div class="col-md-5">
                     <a href="{{ url('boletins') }}" class="btn btn-primary pull-right"><i class="fa fa-file-o"></i> Boletins</a>
                     <a href="{{ url('boletim/'.$boletim->id.'/visualizar') }}" class="btn btn-warning pull-right"><i class="fa fa-eye"></i> Visualizar</a>
-                    <a href="{{ url('boletim/'.$boletim->id.'/enviar') }}" class="btn btn-success pull-right"><i class="fa fa-send"></i> Enviar</a>
+                    <a href="{{ url('boletim/'.$boletim->id.'/enviar') }}" class="btn btn-success pull-right"><i class="fa fa-send"></i> Verificar Envio</a>
                 </div>
             </div>
         </div>
