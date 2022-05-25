8f95d58264de6bdc6b3f0e9699d666da8923b78d
Rafael <rafael@coperve.ufsc.br>
2022-05-25 16:45:03 -0300

    ajuste

--- app/MediaFilteredVw.php
+++ app/MediaFilteredVw.php
@@ -6,6 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class MediaFilteredVw extends Model
 {
-    protected $table = 'medias_filtered_vw';
+    protected $table = 'medias_materialized_filtered_vw';
     
 }
