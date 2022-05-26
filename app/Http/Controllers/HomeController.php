45445ccaff1a2b4c8c6a65e9f725eea6a1b4e555
Robson <robson@coperve.ufsc.br>
2022-05-26 10:52:41 -0300

    Atualização Dashboard Cliente

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -11,6 +11,8 @@ use App\Configs;
 use App\Hashtag;
 use App\Media;
 use App\FbPost;
+use App\MediaFilteredVw;
+use App\MediaRuleFilteredVw;
 use App\MediaTwitter;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
@@ -45,6 +47,8 @@ class HomeController extends Controller
         $clientes = null;
         $periodo_relatorio = null;
         $periodo_padrao = $this->periodo_padrao;
+        $data_inicial = Carbon::now()->subDays($this->periodo_padrao - 1)->format('Y-m-d');
+        $data_final = Carbon::now()->format('Y-m-d');
 
         $ig_comments_total = DB::table('ig_comments')
                                 ->join('medias','medias.id','=','ig_comments.media_id')
@@ -61,11 +65,24 @@ class HomeController extends Controller
         $media_instagram = round((Media::where('client_id',$this->client_id)->count() + $ig_comments_total)/30, 1);
 
         $fb_post_pages_total = DB::table('page_post_term')
-        ->join('terms', 'page_post_term.term_id','=','terms.id')
-        ->where('terms.client_id','=',$this->client_id)
-        ->count();
+                                        ->join('terms', 'page_post_term.term_id','=','terms.id')
+                                        ->where('terms.client_id','=',$this->client_id)
+                                        ->count();
 
         $media_facebook = round((FbPost::where('client_id',$this->client_id)->count() + $fb_comments_total + $fb_post_pages_total)/30, 1);
+
+        $mediaModel = new MediaFilteredVw();
+
+        $fb_total = $mediaModel::where(function($query) {
+            $query->Orwhere('tipo', 'FB_COMMENT')
+            ->Orwhere('tipo', 'FB_PAGE_POST')
+            ->Orwhere('tipo', 'FB_PAGE_POST_COMMENT')
+            ->Orwhere('tipo', 'FB_POSTS');
+        })
+        ->where('client_id', $this->client_id)
+        ->whereBetween('date', [$data_inicial.' 00:00:00',$data_final.' 23:23:59'])
+        ->select('id')
+        ->count();
 
         if($u->hasRole('administradores')){
 
@@ -86,7 +103,7 @@ class HomeController extends Controller
             $terms = Term::with('mediasTwitter')->with('medias')->where('client_id', $this->client_id)->where('is_active',true)->orderBy('term')->get();
 
             $totais = array('total_insta' => Media::where('client_id',$this->client_id)->count() + $ig_comments_total, 
-                            'total_face' => FbPost::where('client_id',$this->client_id)->count() + $fb_comments_total,
+                            'total_face' => $fb_total,
                             'total_twitter' => MediaTwitter::where('client_id',$this->client_id)->count());
 
             return view('dashboard_cliente', compact('users','clientes','totais','hashtags','terms','periodo_relatorio','media_twitter','periodo_padrao'));
