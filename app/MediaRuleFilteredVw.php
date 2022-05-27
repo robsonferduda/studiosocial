66c01e0acab2ce8611701ca91117e9e832f65b5d
Robson <robson@coperve.ufsc.br>
2022-05-27 14:59:25 -0300

    Alteração de Sentimentos

--- app/MediaRuleFilteredVw.php
+++ app/MediaRuleFilteredVw.php
@@ -7,9 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 class MediaRuleFilteredVw extends Model
 {
     protected $table = 'medias_materialized_rule_filtered_vw';
+    public $timestamps = false;
 
     public function rule()
     {
         return $this->hasOne('App\RuleMessage', 'message_id', 'id');
     } 
+
+
 }
