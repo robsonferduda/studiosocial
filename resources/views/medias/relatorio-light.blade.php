d08cc7bc704cf4f3e3864f0ce49283a9ec94f70e
Robson Fernando Duda <robsonferduda@gmail.com>
2022-06-15 19:19:43 -0300

    Relatório

--- resources/views/medias/relatorio-light.blade.php
+++ resources/views/medias/relatorio-light.blade.php
@@ -24,7 +24,8 @@
         <div style="width: 85%; float: left;">
             <h6 style="margin-bottom: 0px; padding-bottom: 5px; margin-top: 26px; font-size: 17px; border-bottom: 3px solid #b5b4b4;">{{ $nome }}</h6>
             <p style="color: #eb8e06; margin: 0;"><strong>Período: {{ $dt_inicial }} à {{ $dt_final }}</strong></p>
-            <p style="color: #eb8e06; margin: 0; margin-top: -3px;">{{ session('cliente')['nome'] }}</p>        
+            <p style="color: #eb8e06; margin: 0; margin-top: -3px;">{{ session('cliente')['nome'] }}</p>   
+            <p style="color: #eb8e06; margin: 0; margin-top: -3px;">FORAM COLETADOS {{ count($dados) }} POSTS</p>     
         </div>
         <div style="width: 15%; float: right; text-align: right;">
             <img style="width: 90%" src="{{ url('img/studio_social.png') }}"/>
