e1babceea569f0344d8f83af46e60cf1d33ffbb2
Rafael <rafael@coperve.ufsc.br>
2022-06-21 11:39:35 -0300

    ajuste

--- app/Classes/FbTerm.php
+++ app/Classes/FbTerm.php
@@ -2,7 +2,9 @@
 
 namespace App\Classes;
 
+use App\Enums\SocialMedia;
 use App\Jobs\FbTerm as JobsFbTerm;
+use App\Term;
 
 class FbTerm{
 
@@ -12,7 +14,12 @@ class FbTerm{
 
     public function runJob()
     {
-        JobsFbTerm::dispatch();                
+        $termos_ativos = Term::where('social_media_id', SocialMedia::FACEBOOK)
+        ->where('is_active',true)->get();
+
+        foreach ($termos_ativos as $termo) {     
+            JobsFbTerm::dispatch($termo);              
+        }  
     }
 
 }
--- app/Jobs/FbTerm.php
+++ app/Jobs/FbTerm.php
@@ -7,25 +7,24 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
-use App\ClientPageMonitor;
-use App\Enums\SocialMedia;
 use App\FbPagePost;
 use App\FbPagePostComment;
-use App\Term;
 
 class FbTerm implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
     public $timeout = 2000;
+    protected $termo;
 
     /**
      * Create a new job instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct($termo)
     {
+        $this->termo = $termo;
     }
 
     /**
@@ -36,35 +35,32 @@ class FbTerm implements ShouldQueue
     public function handle()
     {
         set_time_limit(0);
-        $termos_ativos = Term::where('social_media_id', SocialMedia::FACEBOOK)
-                        ->where('is_active',true)->get();
+                      
+        $termo = $this->termo;
 
-        foreach ($termos_ativos as $termo) {                    
-            $last = $termo->pagePosts()->latest('created_at')->first();
-            $last_comment = $termo->pagePostsComments()->latest('created_at')->first();
-            $posts = FbPagePost::select('id')->where(function ($query) use ($termo) {
-                    $query->where('message', 'ilike', '% '.strtolower($termo->term).' %')
-                        ->orWhere('message', 'ilike', '%'.strtolower($termo->term).' %')
-                        ->orWhere('message', 'ilike', '% '.strtolower($termo->term).'%');
-                    })
-                    ->when($last, function ($q) use ($last){
-                        return $q->where('updated_time', '>=', $last->created_at->subDay()->toDateString());
-                    })                                    
-                    ->get();
-            $termo->pagePosts()->syncWithoutDetaching($posts->pluck('id')->toArray());
-            
-            $comments = FbPagePostComment::select('id')->where(function ($query) use ($termo) {
-                    $query->where('text', 'ilike', '% '.strtolower($termo->term).' %')
-                        ->orWhere('text', 'ilike', '%'.strtolower($termo->term).' %')
-                        ->orWhere('text', 'ilike', '% '.strtolower($termo->term).'%');
-                    })
-                    ->when($last_comment, function ($q) use ($last_comment){
-                        return $q->where('created_time', '>=', $last_comment->created_at->subDay()->toDateString());
-                    })                                    
-                    ->get();
-            $termo->pagePostsComments()->syncWithoutDetaching($comments->pluck('id')->toArray());
-
-        }
-       
+        $last = $termo->pagePosts()->latest('created_at')->first();
+        $last_comment = $termo->pagePostsComments()->latest('created_at')->first();
+        $posts = FbPagePost::select('id')->where(function ($query) use ($termo) {
+                $query->where('message', 'ilike', '% '.strtolower($termo->term).' %')
+                    ->orWhere('message', 'ilike', '%'.strtolower($termo->term).' %')
+                    ->orWhere('message', 'ilike', '% '.strtolower($termo->term).'%');
+                })
+                ->when($last, function ($q) use ($last){
+                    return $q->where('updated_time', '>=', $last->created_at->subDay()->toDateString());
+                })                                    
+                ->get();
+        $termo->pagePosts()->syncWithoutDetaching($posts->pluck('id')->toArray());
+           
+        $comments = FbPagePostComment::select('id')->where(function ($query) use ($termo) {
+                $query->where('text', 'ilike', '% '.strtolower($termo->term).' %')
+                    ->orWhere('text', 'ilike', '%'.strtolower($termo->term).' %')
+                    ->orWhere('text', 'ilike', '% '.strtolower($termo->term).'%');
+                })
+                ->when($last_comment, function ($q) use ($last_comment){
+                    return $q->where('created_time', '>=', $last_comment->created_at->subDay()->toDateString());
+                })                                    
+                ->get();
+        $termo->pagePostsComments()->syncWithoutDetaching($comments->pluck('id')->toArray());
+   
     }
 }
