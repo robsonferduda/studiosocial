5be06f6a2f71c5a56d0d9042e4b68f5c662a3a4c
Robson <robson@coperve.ufsc.br>
2022-06-29 08:27:32 -0300

    BUG Twitter

--- app/Twitter/TwitterCollect.php
+++ app/Twitter/TwitterCollect.php
@@ -48,44 +48,43 @@ class TwitterCollect{
                 "retweeted" => true,
                 "tweet_mode" => "extended"
             );
-
-            $locations = array("id" => 23424768);
-            $tweets = $this->conn->get('trends/place', $locations);
     
             $tweets = $this->conn->get('search/tweets', $query);
-    
-            foreach ($tweets->statuses as $tweet) {
-              
-                $chave = array('twitter_id' => $tweet->id);
-                $dados = array('full_text' => $tweet->full_text,
-                            'retweet_count' => $tweet->retweet_count,
-                            'client_id' => $hashtag->client_id,
-                            'favorite_count' => $tweet->favorite_count,
-                            'user_id' => $tweet->user->id,
-                            'user_name' => $tweet->user->screen_name,
-                            'user_screen_name' => $tweet->user->screen_name,
-                            'user_location' => $tweet->user->location,
-                            'user_followers_count' => $tweet->user->followers_count,
-                            'user_friends_count' => $tweet->user->friends_count,
-                            'user_profile_image_url' => $tweet->user->profile_image_url,
-                            'created_tweet_at' => $tweet->created_at,
-                            'permalink' => 'https://twitter.com/'.$tweet->user->screen_name.'/status/'.$tweet->id,
-                            'place_name' => ($tweet->place and $tweet->place->place_type) ? $tweet->place->name : ''
-                            );
-    
-                $tweet = MediaTwitter::updateOrCreate($chave, $dados); 
-                if($tweet->wasRecentlyCreated) $total++;
-                $tweet->hashtags()->syncWithoutDetaching($hashtag->id);
-            }  
             
-            $dados_coleta = array('id_type_collect' => TypeCollect::HASHTAG,
-                                  'id_social_media' => SocialMedia::TWITTER,
-                                  'id_type_message' => EnumTypeMessage::TWEETS,
-                                  'description' => $hashtag->hashtag,
-                                  'total' => $total,
-                                  'client_id' =>$hashtag->client_id );            
+            if($tweets->statuses){
+                foreach ($tweets->statuses as $tweet) {
+                
+                    $chave = array('twitter_id' => $tweet->id);
+                    $dados = array('full_text' => $tweet->full_text,
+                                'retweet_count' => $tweet->retweet_count,
+                                'client_id' => $hashtag->client_id,
+                                'favorite_count' => $tweet->favorite_count,
+                                'user_id' => $tweet->user->id,
+                                'user_name' => $tweet->user->screen_name,
+                                'user_screen_name' => $tweet->user->screen_name,
+                                'user_location' => $tweet->user->location,
+                                'user_followers_count' => $tweet->user->followers_count,
+                                'user_friends_count' => $tweet->user->friends_count,
+                                'user_profile_image_url' => $tweet->user->profile_image_url,
+                                'created_tweet_at' => $tweet->created_at,
+                                'permalink' => 'https://twitter.com/'.$tweet->user->screen_name.'/status/'.$tweet->id,
+                                'place_name' => ($tweet->place and $tweet->place->place_type) ? $tweet->place->name : ''
+                                );
+        
+                    $tweet = MediaTwitter::updateOrCreate($chave, $dados); 
+                    if($tweet->wasRecentlyCreated) $total++;
+                    $tweet->hashtags()->syncWithoutDetaching($hashtag->id);
+                }  
+                
+                $dados_coleta = array('id_type_collect' => TypeCollect::HASHTAG,
+                                    'id_social_media' => SocialMedia::TWITTER,
+                                    'id_type_message' => EnumTypeMessage::TWEETS,
+                                    'description' => $hashtag->hashtag,
+                                    'total' => $total,
+                                    'client_id' =>$hashtag->client_id );            
 
-            Collect::create($dados_coleta);
+                Collect::create($dados_coleta);
+            }
         }
     }
 
@@ -108,38 +107,40 @@ class TwitterCollect{
             $tweets = $this->conn->get('search/tweets', $query);
 
             $total = 0;    
-            foreach ($tweets->statuses as $tweet) {
-    
-                $chave = array('twitter_id' => $tweet->id);
-                $dados = array('full_text' => $tweet->full_text,
-                            'retweet_count' => $tweet->retweet_count,
-                            'client_id' => $term->client_id,
-                            'favorite_count' => $tweet->favorite_count,
-                            'user_id' => $tweet->user->id,
-                            'user_name' => $tweet->user->screen_name,
-                            'user_screen_name' => $tweet->user->screen_name,
-                            'user_location' => $tweet->user->location,
-                            'user_followers_count' => $tweet->user->followers_count,
-                            'user_friends_count' => $tweet->user->friends_count,
-                            'user_profile_image_url' => $tweet->user->profile_image_url,
-                            'created_tweet_at' => $tweet->created_at,
-                            'permalink' => 'https://twitter.com/'.$tweet->user->screen_name.'/status/'.$tweet->id,
-                            'place_name' => ($tweet->place and $tweet->place->place_type) ? $tweet->place->name : ''
-                            );
-    
-                $tweet = MediaTwitter::updateOrCreate($chave, $dados); 
-                if($tweet->wasRecentlyCreated) $total++;
-                $tweet->terms()->syncWithoutDetaching($term->id);
-            }
-                       
-            $dados_coleta = array('id_type_collect' => TypeCollect::TERMO,
-                                  'id_social_media' => SocialMedia::TWITTER,
-                                  'id_type_message' => EnumTypeMessage::TWEETS,
-                                  'description' => $term->term,
-                                  'total' => $total,
-                                  'client_id' =>$term->client_id );
+            if($tweets->statuses){
+                foreach ($tweets->statuses as $tweet) {
+        
+                    $chave = array('twitter_id' => $tweet->id);
+                    $dados = array('full_text' => $tweet->full_text,
+                                'retweet_count' => $tweet->retweet_count,
+                                'client_id' => $term->client_id,
+                                'favorite_count' => $tweet->favorite_count,
+                                'user_id' => $tweet->user->id,
+                                'user_name' => $tweet->user->screen_name,
+                                'user_screen_name' => $tweet->user->screen_name,
+                                'user_location' => $tweet->user->location,
+                                'user_followers_count' => $tweet->user->followers_count,
+                                'user_friends_count' => $tweet->user->friends_count,
+                                'user_profile_image_url' => $tweet->user->profile_image_url,
+                                'created_tweet_at' => $tweet->created_at,
+                                'permalink' => 'https://twitter.com/'.$tweet->user->screen_name.'/status/'.$tweet->id,
+                                'place_name' => ($tweet->place and $tweet->place->place_type) ? $tweet->place->name : ''
+                                );
+        
+                    $tweet = MediaTwitter::updateOrCreate($chave, $dados); 
+                    if($tweet->wasRecentlyCreated) $total++;
+                    $tweet->terms()->syncWithoutDetaching($term->id);
+                }
+                        
+                $dados_coleta = array('id_type_collect' => TypeCollect::TERMO,
+                                    'id_social_media' => SocialMedia::TWITTER,
+                                    'id_type_message' => EnumTypeMessage::TWEETS,
+                                    'description' => $term->term,
+                                    'total' => $total,
+                                    'client_id' =>$term->client_id );
 
-            Collect::create($dados_coleta);
+                Collect::create($dados_coleta);
+            }
         }
     }
 }
