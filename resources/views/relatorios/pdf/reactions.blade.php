596bad585ae0e4e01eb4178ce988257c760975ba
rafael01costa <rafael01costa@gmail.com>
2022-07-06 18:42:24 -0300

    Ajuste relatórios

--- resources/views/relatorios/pdf/reactions.blade.php
+++ resources/views/relatorios/pdf/reactions.blade.php
@@ -34,7 +34,7 @@
                     @foreach($dados as $reaction)
                         <tr>
                             <td>{{ $reaction->name }}</td>
-                            <td class="center">{!! $reaction->icon !!}</td>
+                            <td class="center">{!! $reaction->icon_html !!}</td>
                             <td class="center">{{ $reaction->count }}</td>
                         </tr>
                     @endforeach
