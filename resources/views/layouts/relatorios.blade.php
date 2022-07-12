268816272c460add88405510c200058047fd0887
Rafael <rafael@coperve.ufsc.br>
2022-07-12 14:17:43 -0300

    ajuste

--- resources/views/layouts/relatorios.blade.php
+++ resources/views/layouts/relatorios.blade.php
@@ -4,7 +4,10 @@
         <meta charset="utf-8" />
         <!--     Fonts and icons     -->
         <!-- CSS Files -->
+        <link href="{{ public_path('css/bootstrap.min.css') }}" rel="stylesheet" />
+        <link href="{{ public_path('css/paper-dashboard.css?v=2.0.1') }}" rel="stylesheet" />
         <!-- CSS Just for demo purpose, don't include it in your project -->
+        <link href="{{ public_path('demo/demo.css') }}" rel="stylesheet" />
         <link href="{{ public_path('css/custom.css') }}" rel="stylesheet" />
     </head>
     <body>
