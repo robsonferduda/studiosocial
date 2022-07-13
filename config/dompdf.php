367c290a4e7481c53e3d79c7417dd09eda19ea91
Rafael Costa <rafael01costa@gmail.com>
2022-07-13 19:40:09 -0300

    Ajuste no relatório de postagens

--- config/dompdf.php
+++ config/dompdf.php
@@ -227,7 +227,7 @@ return array(
          *
          * @var bool
          */
-        "enable_remote" => false,
+        "enable_remote" => true,
 
         /**
          * A ratio applied to the fonts height to be more like browsers' line height
