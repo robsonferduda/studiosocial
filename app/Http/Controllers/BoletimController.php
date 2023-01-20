64b1a2698c69506dca46cc66efd2732aea4352a5
Robson <robson@coperve.ufsc.br>
2023-01-20 12:04:23 -0300

    Boletim

--- app/Http/Controllers/BoletimController.php
+++ app/Http/Controllers/BoletimController.php
@@ -19,7 +19,7 @@ class BoletimController extends Controller
 
     public function index()
     {
-        $boletins = Boletim::whereIn('id_cliente',[443,452])->orderBy('data','DESC')->get();
+        $boletins = Boletim::whereIn('id_cliente',[30,443,452])->paginate(50)->orderBy('data','DESC')->get();
         return view('boletim/index',compact('boletins'));
     }
 
