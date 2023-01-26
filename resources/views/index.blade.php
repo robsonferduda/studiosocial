08eeececc26b5dd54e8b9309b0e393c9b7d3d5c6
Robson Fernando Duda <robsonferduda@gmail.com>
2023-01-26 17:21:17 -0300

    Alterações de Acesso

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,150 +1,175 @@
 @extends('layouts.app')
 @section('content')
     <div class="row">
-        <div class="col-lg-3 col-md-3 col-sm-6">
-            <div class="card card-stats">
-                <div class="card-body ">
-                    <div class="row">
-                        <div class="col-5 col-md-4">
-                            <div class="icon-big text-center icon-warning">
-                                <i class="nc-icon nc-briefcase-24 text-info"></i>
+        @role('boletim')
+            <div class="col-lg-3 col-md-3 col-sm-6">
+                <div class="card card-stats">
+                    <div class="card-body ">
+                        <div class="row">
+                            <div class="col-5 col-md-4">
+                                <div class="icon-big text-center icon-warning">
+                                    <i class="nc-icon nc-briefcase-24 text-info"></i>
+                                </div>
                             </div>
-                        </div>
-                        <div class="col-7 col-md-8">
-                            <div class="numbers">
-                                <p class="card-category">Clientes</p>
-                                <p class="card-title"><a href="{{ url('clientes') }}">{{ $clientes }}</a></p>
+                            <div class="col-7 col-md-8">
+                                <div class="numbers">
+                                    <p class="card-category">Boletins</p>
+                                    <p class="card-title"><a href="{{ url('boletins') }}">Ver</a></p>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div class="card-footer ">
+            </div>  
+        @endrole
+        @role('administradores')
+            <div class="col-lg-3 col-md-3 col-sm-6">
+                <div class="card card-stats">
+                    <div class="card-body ">
+                        <div class="row">
+                            <div class="col-5 col-md-4">
+                                <div class="icon-big text-center icon-warning">
+                                    <i class="nc-icon nc-briefcase-24 text-info"></i>
+                                </div>
+                            </div>
+                            <div class="col-7 col-md-8">
+                                <div class="numbers">
+                                    <p class="card-category">Clientes</p>
+                                    <p class="card-title"><a href="{{ url('clientes') }}">{{ $clientes }}</a></p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card-footer ">
+                            <hr>
+                            <div class="stats">
+                                <a href="{{ url('clientes/create') }}"><i class="fa fa-plus text-info"></i> Novo</a>                
+                            </div>
+                    </div>
+                </div>
+            </div>  
+            <div class="col-lg-3 col-md-3 col-sm-6">
+                <div class="card card-stats">
+                    <div class="card-body ">
+                        <div class="row">
+                            <div class="col-5 col-md-4">
+                                <div class="icon-big text-center icon-warning">
+                                    <i class="fa fa-facebook text-facebook"></i>
+                                </div>
+                            </div>
+                            <div class="col-7 col-md-8">
+                                <div class="numbers">
+                                    <p class="card-category">Média Facebook</p>
+                                    <p class="card-title">{{ $media_facebook }}</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card-footer ">
                         <hr>
-                        <div class="stats">
-                            <a href="{{ url('clientes/create') }}"><i class="fa fa-plus text-info"></i> Novo</a>                
-                        </div>
+                        <span class="text-info"><i class="fa fa-clock-o"></i> Atualizado em {{ date('d/m/Y H:i:s') }}</span>
+                    </div>
                 </div>
-            </div>
-        </div>  
-        <div class="col-lg-3 col-md-3 col-sm-6">
-            <div class="card card-stats">
-                <div class="card-body ">
-                    <div class="row">
-                        <div class="col-5 col-md-4">
-                            <div class="icon-big text-center icon-warning">
-                                <i class="fa fa-facebook text-facebook"></i>
+            </div>  
+            <div class="col-lg-3 col-md-3 col-sm-6">
+                <div class="card card-stats">
+                    <div class="card-body ">
+                        <div class="row">
+                            <div class="col-5 col-md-4">
+                                <div class="icon-big text-center icon-warning">
+                                    <i class="fa fa-instagram text-pink""></i>
+                                </div>
                             </div>
-                        </div>
-                        <div class="col-7 col-md-8">
-                            <div class="numbers">
-                                <p class="card-category">Média Facebook</p>
-                                <p class="card-title">{{ $media_facebook }}</p>
+                            <div class="col-7 col-md-8">
+                                <div class="numbers">
+                                    <p class="card-category">Média Instagram</p>
+                                    <p class="card-title">{{ $media_instagram }}</p>
+                                </div>
                             </div>
                         </div>
                     </div>
+                    <div class="card-footer ">
+                        <hr>
+                        <span class="text-info"><i class="fa fa-clock-o"></i> Atualizado em {{ date('d/m/Y H:i:s') }}</span>
+                    </div>
                 </div>
-                <div class="card-footer ">
-                    <hr>
-                    <span class="text-info"><i class="fa fa-clock-o"></i> Atualizado em {{ date('d/m/Y H:i:s') }}</span>
-                </div>
-            </div>
-        </div>  
-        <div class="col-lg-3 col-md-3 col-sm-6">
-            <div class="card card-stats">
-                <div class="card-body ">
-                    <div class="row">
-                        <div class="col-5 col-md-4">
-                            <div class="icon-big text-center icon-warning">
-                                <i class="fa fa-instagram text-pink""></i>
+            </div>    
+            <div class="col-lg-3 col-md-3 col-sm-6">
+                <div class="card card-stats">
+                    <div class="card-body ">
+                        <div class="row">
+                            <div class="col-5 col-md-4">
+                                <div class="icon-big text-center icon-warning">
+                                    <i class="fa fa-twitter text-info"></i>
+                                </div>
                             </div>
-                        </div>
-                        <div class="col-7 col-md-8">
-                            <div class="numbers">
-                                <p class="card-category">Média Instagram</p>
-                                <p class="card-title">{{ $media_instagram }}</p>
+                            <div class="col-7 col-md-8">
+                                <div class="numbers">
+                                    <p class="card-category">Média de Tweets</p>
+                                    <p class="card-title"><a>{{ $media_twitter }}</a></p>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
-                <div class="card-footer ">
-                    <hr>
-                    <span class="text-info"><i class="fa fa-clock-o"></i> Atualizado em {{ date('d/m/Y H:i:s') }}</span>
-                </div>
-            </div>
-        </div>    
-        <div class="col-lg-3 col-md-3 col-sm-6">
-            <div class="card card-stats">
-                <div class="card-body ">
-                    <div class="row">
-                        <div class="col-5 col-md-4">
-                            <div class="icon-big text-center icon-warning">
-                                <i class="fa fa-twitter text-info"></i>
-                            </div>
-                        </div>
-                        <div class="col-7 col-md-8">
-                            <div class="numbers">
-                                <p class="card-category">Média de Tweets</p>
-                                <p class="card-title"><a>{{ $media_twitter }}</a></p>
-                            </div>
-                        </div>
+                    <div class="card-footer ">
+                        <hr>
+                        <span class="text-info"><i class="fa fa-clock-o"></i> Atualizado em {{ date('d/m/Y H:i:s') }}</span>
                     </div>
                 </div>
-                <div class="card-footer ">
-                    <hr>
-                    <span class="text-info"><i class="fa fa-clock-o"></i> Atualizado em {{ date('d/m/Y H:i:s') }}</span>
-                </div>
-            </div>
-        </div>   
-    </div> 
-    <div class="row">
-        <div class="col-lg-4 col-md-4 col-sm-6">
-            <div class="card card-stats" id='cloud_card'>
-                <div class="card-body ">
-                    <div class="row">
-                        <div class="col-lg-12 col-md-12 col-sm-12">
-                            <div class="custom-cloud" id='cloud'></div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            </div>   
         </div> 
+    @endrole
+    @role('administradores')
+        <div class="row">
+            <div class="col-lg-4 col-md-4 col-sm-6">
+                <div class="card card-stats" id='cloud_card'>
+                    <div class="card-body ">
+                        <div class="row">
+                            <div class="col-lg-12 col-md-12 col-sm-12">
+                                <div class="custom-cloud" id='cloud'></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div> 
 
-        <div class="col-lg-4 col-md-4 col-sm-6">
-            <div class="card card-stats">
-                <div class="card-body ">
-                    <div class="row">
-                        <div class="col-12 col-md-12">
-                            <h6 class="mt-2 mb-3">
-                                Termos Ativos
-                                <a class="pull-right" href="{{ url('terms/client', session('cliente')['id']) }}"><i class="fa fa-plus"></i> Adicionar</a>
-                            </h6>
-                            @foreach($terms as $term)
-                                <p>{{ $term->term }}</p>
-                            @endforeach
+            <div class="col-lg-4 col-md-4 col-sm-6">
+                <div class="card card-stats">
+                    <div class="card-body ">
+                        <div class="row">
+                            <div class="col-12 col-md-12">
+                                <h6 class="mt-2 mb-3">
+                                    Termos Ativos
+                                    <a class="pull-right" href="{{ url('terms/client', session('cliente')['id']) }}"><i class="fa fa-plus"></i> Adicionar</a>
+                                </h6>
+                                @foreach($terms as $term)
+                                    <p>{{ $term->term }}</p>
+                                @endforeach
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        </div>   
-  
-        <div class="col-lg-4 col-md-4 col-sm-6">
-            <div class="card card-stats">
-                <div class="card-body ">
-                    <div class="row">
-                        <div class="col-12 col-md-12">
-                            <h6 class="mt-2 mb-3">
-                                Hashtags Ativas
-                                <a class="pull-right" href="{{ url('client/hashtags', session('cliente')['id']) }}"><i class="fa fa-plus"></i> Adicionar</a>
-                            </h6>
-                            @foreach($hashtags as $hashtag)
-                                <p>#{{ $hashtag->hashtag }}</p>
-                            @endforeach
+            </div>   
+    
+            <div class="col-lg-4 col-md-4 col-sm-6">
+                <div class="card card-stats">
+                    <div class="card-body ">
+                        <div class="row">
+                            <div class="col-12 col-md-12">
+                                <h6 class="mt-2 mb-3">
+                                    Hashtags Ativas
+                                    <a class="pull-right" href="{{ url('client/hashtags', session('cliente')['id']) }}"><i class="fa fa-plus"></i> Adicionar</a>
+                                </h6>
+                                @foreach($hashtags as $hashtag)
+                                    <p>#{{ $hashtag->hashtag }}</p>
+                                @endforeach
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        </div>   
-    </div>
+            </div>   
+        </div>
+    @endrole
 @endsection
 @section('script')
 <script>
