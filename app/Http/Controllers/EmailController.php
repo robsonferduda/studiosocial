40c6080684976e2d80e57acd45793aa1ae0f68e2
Robson <robson@coperve.ufsc.br>
2023-04-18 14:07:40 -0300

    Verificação Erro

--- app/Http/Controllers/EmailController.php
+++ app/Http/Controllers/EmailController.php
@@ -14,7 +14,7 @@ class EmailController extends Controller
 {
     public function __construct()
     {
-        $this->middleware('auth');
+        //$this->middleware('auth');
     }
 
     public function index()
