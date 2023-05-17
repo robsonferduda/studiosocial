9c0ceca66e87f7a684feb18a9eafcbc389093e75
Robson Fernando Duda <robsonferduda@gmail.com>
2023-05-16 21:39:18 -0300

    Bug Link

--- app/Http/Controllers/BoletimController.php
+++ app/Http/Controllers/BoletimController.php
@@ -310,6 +310,8 @@ class BoletimController extends Controller
                 $url = env('FILE_URL').$noticia->clipagem.'/arquivo'.$noticia->id.'_1.jpg';
                 $header_response = @get_headers($url, 1);
 
+                dd(strpos( $header_response[0], "404" ) !== false or strpos( $header_response[0], "301" ) !== false);
+
                 dd($header_response);
 
                 if(strpos( $header_response[0], "404" ) !== false or strpos( $header_response[0], "301" ) !== false){
