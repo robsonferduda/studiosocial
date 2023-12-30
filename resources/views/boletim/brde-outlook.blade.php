2757ab1b4ec0fcb8dc583db116b5c004650e9996
Robson Fernando Duda <robsonferduda@gmail.com>
2023-12-29 23:04:05 -0300

    Atualização Boletim

--- resources/views/boletim/brde-outlook.blade.php
+++ resources/views/boletim/brde-outlook.blade.php
@@ -93,12 +93,14 @@
                     @if($noticia->uf != $uf)
                         <div class="px-3 py-2 mb-3" style="">
                             <div style="text-transform: uppercase; font-weight: 600; border-bottom: 1px solid #52c9ff;">
+                                <div style="margin-left: -10px;">
                                 <table style="margin-left: -10px;">
                                     <tr>
                                     <td><img class="icone" style="margin-left: -10px;" src="https://boletim.studioclipagem.com.br/img/map.png"></td>
                                     <td><p>{!! $uf_extenso !!}</p></td>
                                     </tr>
                                 </table> 
+                                </div>
                             </div>
                         @php
                             $flag_uf = true;
