a8ed8c0964b9639df6cff5c9efa996de2dc52b0f
Robson Fernando Duda <robsonferduda@gmail.com>
2024-08-15 15:22:04 -0300

    Coletas

--- resources/views/octoparse/index.blade.php
+++ resources/views/octoparse/index.blade.php
@@ -4,34 +4,28 @@
     <div class="card">
         <div class="card-header">
             <div class="row">
-                <div class="col-md-8">
+                <div class="col-md-6">
                     <h4 class="card-title">
                         <i class="fa fa-plug ml-3"></i> Octoparse 
                         <i class="fa fa-angle-double-right" aria-hidden="true"></i> Importação de Dados
                     </h4>
                 </div>
-                <div class="col-md-4">
-                    
+                <div class="col-md-6">
+                    {!! Form::open(['id' => 'frm-pautas', 'class' => 'form-horizontal', 'url' => ['octoparse/importar']]) !!}
+                        <div class="form-group m-3 w-70">
+                            <div class="row">
+                                <div class="col-md-12 center">
+                                    <button style="position: absolute; right: 20px; top: -20px;" type="submit" id="btn-find" class="btn btn-primary mt-4"><i class="fa fa-download"></i> Importar Manualmente</button>
+                                </div>
+                            </div>
+                        </div>
+                    {!! Form::close() !!}
                 </div>
             </div>
         </div>
         <div class="card-body">
             <div class="col-md-12">
                 @include('layouts.mensagens')
-            </div>
-            <div class="row">
-                <div class="col-lg-4 col-sm-4">
-                    {!! Form::open(['id' => 'frm-pautas', 'class' => 'form-horizontal', 'url' => ['octoparse/importar']]) !!}
-                        <div class="form-group m-3 w-70">
-                            <div class="row">
-                                <div class="col-md-12 center">
-                                    <button type="submit" id="btn-find" class="btn btn-primary mt-4"><i class="fa fa-download"></i> Importar Manualmente</button>
-                                </div>
-                            </div>
-                        </div>
-                    {!! Form::close() !!}
-                </div>             
-               
             </div>
             <div class="row">
                 <div class="col-lg-8 col-sm-8 ml-3">
